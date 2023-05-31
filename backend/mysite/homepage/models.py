5a756560abd6bdad9cec1a6401e2c6598689a0b7
moringspeaker <guchenyu150533@gmail.com>
2023-05-31 02:35:15 -0400

    add blogs

--- backend/mysite/homepage/models.py
+++ backend/mysite/homepage/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- backend/mysite/homepage/views.py
+++ backend/mysite/homepage/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
+from .models import Category
 
 def index_view(request):
-    if not request.user.is_authenticated:
-        return render(request, "homepage/index.html", {"page": "home"})
-
-    return render(
-        request,
-        "homepage/index.html",
-        {"firstname": "{}".format(request.user.first_name), "page": "home"},
-    )
+    blogs = Blog.objects.all()
+    return render(request, 'index.html', {'blog': blogs})
