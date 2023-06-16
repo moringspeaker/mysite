be14e694c733f0131fd68fb13ddc52461853c317
moringspeaker <guchenyu150533@gmail.com>
2023-06-16 01:46:06 -0400

    cross domain access

--- frontend/myfront/vue.config.js
+++ frontend/myfront/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000', // Replace with your Django backend URL
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
 })
