40f7d6c9303a504f971f2714cb32228ab0a738b4
moringspeaker <guchenyu150533@gmail.com>
2023-06-17 00:37:47 -0400

    Initially realize the cross-domain

--- backend/mysite/homepage/models.py
+++ backend/mysite/homepage/models.py
@@ -7,6 +7,6 @@ class Swiper(models.Model):
     id = models.AutoField(primary_key=True)
     ENtitle = models.CharField(max_length=200,blank=True)
     CHtitle = models.CharField(max_length=200, blank=True)
-    img = models.ImageField(upload_to='medias/swipers/', default='static/no-img.jpg')
+    src = models.ImageField(upload_to='medias/swipers/', default='static/no-img.jpg')
     def __str__(self):
         return self.ENtitle
