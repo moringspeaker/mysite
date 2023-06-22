8b2249c7447892235be6ace7265706c4146006f6
moringspeaker <guchenyu150533@gmail.com>
2023-06-22 01:09:55 -0400

    blogs api

--- backend/mysite/blogs/views.py
+++ backend/mysite/blogs/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 from .models import Category
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import BlogsSerializer
-
+from django.http import JsonResponse
 
 class BlogsAPIView(generics.ListAPIView):
     queryset = Blog.objects.all()
@@ -13,7 +13,20 @@ class BlogsAPIView(generics.ListAPIView):
     pagination_class.default_limit = 5
 
 class BlogDetailView(generics.RetrieveAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogsSerializer
+
+    def get_all_categories(self):
+        return Category.objects.all()
+    def get_blogs_by_category(self, category):
+        return Blog.objects.filter(category=category)
+
+    def return_all_blog(self):
+        categories = self.get_all_categories()
+        blogs = {}
+        for category in categories:
+            blogs[category.name] = self.get_blogs_by_category(category)
+
+        return JsonResponse(blogs)
+
+
 
 
