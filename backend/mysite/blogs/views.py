6b9b881ef2465f81d56b34784097b2699166f4cb
moringspeaker <guchenyu150533@gmail.com>
2023-06-26 03:18:41 -0400

    finish return single blog

--- backend/mysite/blogs/views.py
+++ backend/mysite/blogs/views.py
@@ -1,71 +1,26 @@
 from .models import Blog
 from .models import Category
 from .models import Collection
-from rest_framework.pagination import LimitOffsetPagination
-from .serializers import BlogsSerializer
 from rest_framework import generics
-from rest_framework.response import Response
 from collections import defaultdict
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Blog
+from .serializers import BlogsSerializer
 
-
-
-class BlogsAPIView(generics.ListAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogsSerializer
-    pagination_class = LimitOffsetPagination
-    pagination_class.default_limit = 5
-
-    def format_created_date(self, blog):
-        return blog.formatted_created_date()
-
-    def list(self, request, *args, **kwargs):
-        blogs = self.paginate_queryset(self.queryset)
-
-        formatted_blogs = []
-        for blog in blogs:
-            formatted_blog = self.format_created_date(blog)
-            formatted_blogs.append(formatted_blog)
-
-        serializer = self.get_serializer(formatted_blogs, many=True)
-        return self.get_paginated_response(serializer.data)
-
-
-
+class BlogDetailView(APIView):
+    def get(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        serializer = BlogsSerializer(blog)
+        return Response(serializer.data)
 
 class BlogListView(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogsSerializer  # replace this with your actual serializer
 
-    # def list(self, request):
-    #     data = {}
-    #
-    #     categories = Category.objects.all()
-    #     data_category={}
-    #     for category in categories:
-    #         categroy_blogs = []
-    #         data_category[category.name] = defaultdict(list)  # Create a default dictionary with category name
-    #         blogs = Blog.objects.filter(category=category)
-    #         for blog in blogs:
-    #           if blog.collection is None:   # If the blog does not belong to any collection, add it to the category
-    #             categroy_blogs.append(blog)
-    #         data_category[category.name] = categroy_blogs
-    #
-    #     collections = Collection.objects.all()
-    #     data_collection={}
-    #     for collection in collections:
-    #         collection_blog = []
-    #         data_collection[collection.name] = defaultdict(list)
-    #         blogs = Blog.objects.filter(collection=collection)
-    #         for blog in blogs:
-    #             collection_blog.append(blog)
-    #         data_collection[collection.name] = collection_blog
-    #
-    #     data['category'] = data_category
-    #     data['collection'] = data_collection
-    #
-    #     return Response(data)
-
     def list(self, request):
+
         data = {}
 
         categories = Category.objects.all()
