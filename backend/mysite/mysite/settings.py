fc60658357d1cd0157c28681106ae8d4f9269900
moringspeaker <guchenyu150533@gmail.com>
2023-07-15 00:42:22 -0400

    change debug method

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '137.184.192.173','164.90.253.90','localhost']
 
