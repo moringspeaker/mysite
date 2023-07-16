3efd935ca36852cbad5462b7697f62b8e9402f15
moringspeaker <guchenyu150533@gmail.com>
2023-07-16 18:46:10 -0400

    test

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -153,11 +153,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = False # This should be False for credentials to work
 CORS_ALLOW_ALL_ORIGINS = False
-# CORS_ORIGIN_WHITELIST = (
-#   'http://localhost',
-#   'http://127.0.0.1',
-#   'http://164.90.253.90',
-# )
 
 # 允许所有 域名/IP 跨域
 # CORS_ALLOW_ALL_ORIGINS = True
